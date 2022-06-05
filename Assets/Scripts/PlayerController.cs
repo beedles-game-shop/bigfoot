ff018a227298af000a59e57e1a2116e6eb46392d
Madison Hanberry <mhanberry1@gmail.com>
2022-06-04 22:46:14 -0700

    add grabbing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject grabTrigger;
     public float speed = 1;
+    public float grabRadius = 1;
 
-    private Rigidbody rigidBody;
     private Vector2 movement;
+    private bool isHoldingObject = false;
+    private GameObject heldObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -26,14 +28,54 @@ public class PlayerController : MonoBehaviour
     // Called once at the end of every frame
     private void FixedUpdate()
     {
+        // Move the player
         Vector3 translation = new Vector3(movement.x, 0.0f, movement.y);
 
-        rigidBody.transform.Translate(speed * 0.01f * translation);
+        gameObject.transform.Translate(speed * 0.01f * translation);
+
+        // If an object is being held, move it with the player
+        if(isHoldingObject)
+        {
+            heldObject.transform.position = gameObject.transform.position;
+        }
     }
 
-    // Called when the input system fires an event
+    // Called when the input system fires a "Move" event
     private void OnMove(InputValue inputValue)
     {
         movement = inputValue.Get<Vector2>();
+    }
+
+    // Called when the input system fires a "Grab" event
+    private void OnGrab()
+    {
+        Vector3 playerPosition = gameObject.transform.position;
+
+        // If the player is holding an object, drop it
+        if (isHoldingObject)
+        {
+            isHoldingObject = false;
+            heldObject.transform.position = playerPosition + new Vector3(0, 0, grabRadius);
+            heldObject.GetComponent<BoxCollider>().enabled = true;
+            heldObject.GetComponent<Rigidbody>().isKinematic = false;
+
+            return;
+        }
+
+        // Check for reachable objects
+        Collider[] reachableObjects = Physics.OverlapSphere(playerPosition, grabRadius);
+
+        for (int i = 0; i < reachableObjects.Length; i++)
+        {
+
+            // If the object can be grabbed, pick it up
+            if (reachableObjects[i].gameObject.tag == "Grab")
+            {
+                isHoldingObject = true;
+                heldObject = reachableObjects[i].gameObject;
+                heldObject.GetComponent<BoxCollider>().enabled = false;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
     }
 }
